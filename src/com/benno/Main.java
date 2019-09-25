64160b821edacd428ee0afe8efd0da002edad523
BennoAlif <bennoalif41@gmail.com>
2019-09-25 15:58:41 +0700

    Initial Commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,26 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : mendeklarasikan variabel integer
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000l;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("integer : " + i);
+        System.out.println("long : " + l);
+    }
+}
